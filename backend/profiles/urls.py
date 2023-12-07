324a5180709e7f374ca65bca7bdfb01498e33a41
SarahMainguet <80517150+SarahMainguet@users.noreply.github.com>
2023-12-07 16:41:02 +0100

    added profile viewset

--- backend/profiles/urls.py
+++ backend/profiles/urls.py
@@ -1,9 +1,9 @@
 from rest_framework import routers
 from django.urls import path, include
-# from .views import ItemViewSet
+from .views import ProfileViewSet
 
 router = routers.DefaultRouter()
-# router.register(r'items', ItemViewSet)
+router.register(r'profiles', ProfileViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
